c5ebcf2e5af5120885aada4c890cd78526504037
Ashwin <ashwin.dinesh@webklipper.com>
2019-03-28 19:34:13 +0530

    removes unwanted config settings

--- app/src/main/java/com/webengage/gtm/sample/MainApplication.java
+++ app/src/main/java/com/webengage/gtm/sample/MainApplication.java
@@ -2,10 +2,8 @@ package com.webengage.gtm.sample;
 
 import android.app.Application;
 
-import com.webengage.sdk.android.LocationTrackingStrategy;
 import com.webengage.sdk.android.WebEngageActivityLifeCycleCallbacks;
 import com.webengage.sdk.android.WebEngageConfig;
-import com.webengage.sdk.android.actions.database.ReportingStrategy;
 
 public class MainApplication extends Application {
     @Override
@@ -14,12 +12,6 @@ public class MainApplication extends Application {
         WebEngageConfig config = new WebEngageConfig.Builder()
                 .setWebEngageKey("YOUR-WEBENGAGE-LICENSE-CODE")
                 .setDebugMode(true)
-                .setAutoGCMRegistrationFlag(false)
-                .setEventReportingStrategy(ReportingStrategy.BUFFER)
-                .setPushSmallIcon(R.mipmap.ic_launcher_round)
-                .setPushLargeIcon(R.mipmap.ic_launcher)
-                .setEveryActivityIsScreen(true)
-                .setLocationTrackingStrategy(LocationTrackingStrategy.ACCURACY_CITY)
                 .build();
         registerActivityLifecycleCallbacks(new WebEngageActivityLifeCycleCallbacks(this, config));
     }
